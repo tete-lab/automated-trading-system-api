a7da74489bd8da96a4c04edaa621c56a54a2dd31
kimtg <ktgstar@gmail.com>
2026-02-05 18:03:29 +0900

    EnableScheduling

--- src/main/kotlin/com/ats/server/AutomatedTradingSystemApiApplication.kt
+++ src/main/kotlin/com/ats/server/AutomatedTradingSystemApiApplication.kt
@@ -3,7 +3,9 @@ package com.ats.server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @EnableJpaAuditing //날짜자동입ㄺ
 @SpringBootApplication
 class AutomatedTradingSystemApiApplication
